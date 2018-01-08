6c49fe29b19ec4f30b313ad280ed5b42ec37115b
Cameron <cameron.grams.2@gmail.com>
2018-01-08 07:52:23 -0500

    fix

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const { myData } = require( './data/modelData' );
 const app = express();
 
 app.use(bodyParser.json());
-
+  
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
