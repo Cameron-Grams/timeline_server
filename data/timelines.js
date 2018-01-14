4ccb960a179392d47715dbb409249ae44748e8fd
cameron <cameron.grams.2@gmail.com>
2018-01-14 06:49:42 -0500

    build a good data set

--- data/timelines.js
+++ data/timelines.js
@@ -1,5 +1,12 @@
+const dateEntry = {
+    month: 0,
+    day: 0,
+    year: 1969
+};
+
+
  const singleEntry = {
-    when: new Date(),
+    when: dateEntry,
     where: 'new york',
     who: 'Anthony Bourdain',
     what: 'brought food culture mainstream',
@@ -8,24 +15,24 @@
 
 const timelines = [
     { userId: '123',
-      theme: 'History of Rock n Roll',
+      theme: 'History of US Financial Activity',
       entries: [
         {
-            when: new Date(),
+            when: { month:  , day:  , year:    },
             where: 'new york',
             who: 'Ramones',
             what: 'brought Punk Rock Mainstream',
             why: 'improved mainstream music'
         },
         {
-            when: new Date() + 100,
+            when: { month:  , day:  , year:    },
             where: 'new york',
             who: 'Ramones',
             what: 'brought Punk Rock Mainstream',
             why: 'improved mainstream music'
         },
         {
-            when: new Date() + 200,
+            when: { month:  , day:  , year:    }, 
             where: 'new york',
             who: 'Ramones',
             what: 'brought Punk Rock Mainstream',
@@ -63,3 +70,119 @@ const timelines = [
 ];
 
 module.exports = timelines; 
+
+
+
+
+First known derivatives contract
+what: First known derivative contract: Japanese futures contract for the purchase of rice
+when: 1697
+who: unknown
+where: Japan
+why: First example of a contract for pre-established prices being applied to a future sale
+source: https://www.investopedia.com/articles/optioninvestor/10/history-options-futures.asp
+
+
+First US derivative contract: 
+what: First known US futures sale of grain
+when: March 13, 1851
+who: unknown
+where: Chicago
+why: first use of a future contract in the US
+source: http://eh.net/encyclopedia/a-history-of-futures-trading-in-the-united-states/
+
+
+Chicago Board of Trade established
+what: Chicago Board of Trade established as a place to resolve contracts for 
+      agricultural production
+when: 1848
+who: Chicago merchants
+where: Chicago
+why: This an early example of the financial activity around the time sensitive sale of
+     agricultural products being actively managed
+source: http://eh.net/encyclopedia/a-history-of-futures-trading-in-the-united-states/
+
+Panic of 1907
+what: Attempt by Heinze family and Knickerbocker Trust to corner the copper market led to a run
+      on the banks due to a loss of investor confidence
+when: October 1907
+who: Otto Heinze and Knickerbocker Trust
+where: New York
+why: The attempt to corner cooper market was based on leveraged purchase of short sales of copper;
+     once Otto Heinze was unable to meet his leveraged obligations the loss of confidence in 
+     financial institutions led to a run on the banks as people tried to secure their money
+source: https://en.wikipedia.org/wiki/Panic_of_1907#cite_ref-3
+
+Stock Market Crash 1929
+what: 16 million shares traded on New York Stock exchange as loss of confidence led to market
+      collapse
+when: October 29, 1929
+who: many
+where: New York
+why: Stock prices had become higher than the economic activity they represented due to wide spread
+     speculation; exacerbated by unsustainable debt held by the banks; led to the Glass-Steigel
+     act as an effort to separate speculation from customer holdings
+source: http://www.history.com/topics/1929-stock-market-crash
+
+Glass-Steagall act passed
+what: National legislation passed to separate commercial and investment banking
+when: 1933
+who: Senators Carter Glass and Rep Henry B. Steagall
+where: Washington, DC
+why: Speculation by investment banks had led to the leverage and elevated stock prices that caused
+     the 1929 crash, this act was an effort to protect the citizens from the impacts of bank 
+     investment activity
+source: https://en.wikipedia.org/wiki/Glass%E2%80%93Steagall_legislation
+
+Black Monday 1987
+what: Massive sell-off of stocks leads to a 23% fall in the US stock market
+when: October 19, 1987
+who: many
+where: New York, London, Hong Kong, Berlin, Tokyo
+why: stock prices had outpaced earning potential and sales started to adjust to this reality;
+     automated systems initiated sell orders once the inital sell off started; portfolio
+     insurance was another new innovation that had fostered previous investor confidence
+source: https://www.investopedia.com/ask/answers/042115/what-caused-black-monday-stock-market-crash-1987.asp
+
+
+Glass-Steagall repealed
+what: President Clinton signs the Gramm–Leach–Bliley Act (GLBA) removing barriers between 
+      commercial banking, investment banking, and insurance activity. 
+when: November 12, 1999
+who: Sen Phil Gramm, Rep Jim Leach, Rep Thomas Bliley, and President Bill Clinton
+where: Washington DC
+why: This act increased the degree that the US financial market was interconnected, contributing#
+     to greater systemic risk from contagion
+source: https://en.wikipedia.org/wiki/Gramm%E2%80%93Leach%E2%80%93Bliley_Act
+
+
+2007-8 Market Crash
+what: World-wide financial liquidity disappeared due to a loss of confidence in the value of the 
+      goods that supported financial contracts
+when: September 2008
+who: many
+where: worldwide
+why: Loss of confidence in the system of finance led to a withdraw of money from the system; the 
+     process became a self-fulfilling prophecy; financial markets were exposed to further risk
+     from the adoption of little understood instruments such as CDOs, credit default swaps, and 
+     the integrity of mortgage back securities 
+source: http://www.nytimes.com/2008/09/30/business/30markets.html
+
+Dodd-Frank Act
+what: Reform act designed to protect consumers from bank risk
+when: July 21, 2020
+who: Rep Barney Frank, Sen Chris Dodd, and President Barak Obama
+where: Washington, DB
+why: Greater regulation of the financial markets was seen as the best way to protect the world 
+     from little understood financial risk
+source: https://en.wikipedia.org/wiki/Dodd%E2%80%93Frank_Wall_Street_Reform_and_Consumer_Protection_Act
+
+
+Derivatives move onto Blockchain
+what: Ledger X approved for derivatives trading with digital contracts
+who: Ledger X ( private blockchain trading company);
+     US Commodities Futures Trading Commission ( US CFTC )
+when: 24 July 2017
+where: Washington DC ( US Commodities Futures Trading Commission )/New York ( Ledger X )
+why: This opens the way for blockchain's use in recording derivatives contracts
+source: https://www.reuters.com/article/us-usa-cftc-digitalcurrency/ledgerx-gets-u-s-approval-for-derivatives-on-digital-currencies-idUSKBN1A92FZ
