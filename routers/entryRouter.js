812316f3e1959e7cb23ab0c6e648ce4881db0319
Cameron <cameron.grams.2@gmail.com>
2018-02-26 10:05:42 -0500

    with using the entry ids for delete, move to update and add

--- routers/entryRouter.js
+++ routers/entryRouter.js
@@ -10,7 +10,7 @@ const { entry } = require( '../models/entryModel' );
 const { timeline } = require( '../models/timelineModel' ); 
 
 // adds entries to the timeline by the timeline Id
-router.route('/entries/:timelineId')
+router.route('/entries/:entryId')
     .post( passport.authenticate('jwt', { session: false }), (req, res) => { 
         const dateArray = req.body.date.split( '/' );
         const recordDate = new Date( dateArray[ 2 ], dateArray[ 0 ], dateArray[ 1 ] ); 
@@ -41,15 +41,13 @@ router.route('/entries/:timelineId')
         } )
         .catch( err => res.json( err ) ); 
     })
-    
-router.route( '/entries/update/:timelineId')
    .put( passport.authenticate('jwt', { session: false }), (req, res) => { 
        console.log( '[ entryROuter ] PUT to update entry with ', req.body );
 
         entry.updateOne(
             { _id: req.params.timelineId,
-            "Entries": { $elemMatch: { "_id": { $eq: req.body._id } } } },
-            { $set: { "Entries.$": { 
+            "entries": { $elemMatch: { "_id": { $eq: req.body._id } } } },
+            { $set: { "entries.$": { 
                 title: req.body.title,
                 what: req.body.what,
                 dateObject: req.body.dateObject,
@@ -61,9 +59,70 @@ router.route( '/entries/update/:timelineId')
              } } }
         )
         .then( entry => {
-            return res.json( entry ) 
+            console.log( '[ entryRouter ] updated entry ', entry ); 
+            return res.json( entry );  
+        } )
+        .catch( err => res.json( err ) ); 
+    } )
+
+    // return auth... 
+  .delete( (req, res) => { 
+        timeline.update(
+            { "entries": req.params.entryId },
+            { "$pull": { "entries": req.params.entryId } },
+        ).then(() => entry.findByIdAndRemove(req.params.entryId))
+        .then(
+            () => res.status( 204 ).send()
+        )
+        .catch(err => res.json(err))
+    });
+
+
+
+
+
+ /*   
+router.route( '/entries/update/:timelineId')
+   .put( passport.authenticate('jwt', { session: false }), (req, res) => { 
+       console.log( '[ entryROuter ] PUT to update entry with ', req.body );
+
+        entry.updateOne(
+            { _id: req.params.timelineId,
+            "entries": { $elemMatch: { "_id": { $eq: req.body._id } } } },
+            { $set: { "entries.$": { 
+                title: req.body.title,
+                what: req.body.what,
+                dateObject: req.body.dateObject,
+                date: req.body.date,
+                who: req.body.who,
+                where: req.body.where,
+                content: req.body.content,
+                source: req.body.source
+             } } }
+        )
+        .then( entry => {
+            console.log( '[ entryRouter ] updated entry ', entry ); 
+            return res.json( entry );  
         } )
         .catch( err => res.json( err ) ); 
     } );
+
+router.route( '/entries/delete/:timelineId')
+   .delete( passport.authenticate('jwt', { session: false }), (req, res) => { 
+       console.log( '[ entryRouter ] delete to update entry with ', req.body );
+
+        entry.delete(
+            { _id: req.params.entryId }, { } 
+        )
+        .then(
+            timeline.findByIdAndRemove(
+                { "entries.$": { $elemMatch: { "_id": req.body._id } } }, { }
+            )
+            .then( timeline => { return res.json( timeline ) } )
+            .catch( err => res.json( err ) )
+        )
+        .catch( err => res.json( err ) ); 
+    } );
+*/
 
 module.exports = router;
