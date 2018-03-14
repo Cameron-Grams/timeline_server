120970a2b1aaca78479ac4cd92bb230fb6e5ac95
cameron <cameron.grams.2@gmail.com>
2018-03-13 22:36:58 -0400

    fix the entry tests

--- test/test-entryRouter-integration.js
+++ test/test-entryRouter-integration.js
@@ -6,23 +6,25 @@ const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
 const { user } = require('../models/userModel');
 const { timeline } = require('../models/timelineModel');
+const { entry } = require('../models/entryModel');
 
 chai.use( chaiHttp );
 
 const { app, runServer, closeServer } = require( '../index' );
 const { PORT, TEST_DATABASE_URL, SECRET } = require( '../Config/config' );
 
-describe( "Tests the Timeline Router functionality", () => {
+describe( "Tests the Entry Router functionality", () => {
     
     let accessibleUser;
     let accessibleTimelineId; 
+    let accessibleEntry;
 
     function tearDownDb() {   
-        return new Promise ((resolve, reject) => {     
-            console.warn('Deleting timelines database');     
+        return new Promise (( resolve, reject ) => {     
+            console.warn( 'Deleting timelines database' );     
             mongoose.connection.dropDatabase()       
-            .then(result => resolve(result))       
-            .catch(err => reject(err));   }); 
+            .then( result => resolve( result ) )       
+            .catch( err => reject( err ) );   }); 
         }
 
     before( () => {
@@ -37,9 +39,23 @@ describe( "Tests the Timeline Router functionality", () => {
             email: "testtest@here.com",
             password: "readyplayertwo"
         };
+
+        const recordDate = Date.now(); 
+        const testEntry = {
+            title: "something important",
+            what: "something cool",
+            dateObject: recordDate,
+            date: "1/1/2020",
+            who: "someone",
+            where: "somewhere",
+            content: "something",
+            source: "someplace"
+        }
         
         return user.create( testUserObject )
             .then( ( testUser ) => { 
+                console.log( ' are we in test user? ', testUser._id );
+
                 accessibleUser = testUser; 
                 return testUser;
             } )
@@ -53,15 +69,28 @@ describe( "Tests the Timeline Router functionality", () => {
                     .then( createdTimeline => { 
                         user.findByIdAndUpdate( accessibleUser._id, { $push: { userTimelines: createdTimeline._id } }, { new: true } )
                             .then( () => {
-                                console.log( 'beginning working timeline id is now ', createdTimeline._id );
                                 accessibleTimelineId = createdTimeline._id; 
-                                return createdTimeline; 
+                                entry.create( testEntry )
+                                .then( ( entry ) => {
+                                    timeline.findByIdAndUpdate( { "_id": createdTimeline._id },
+                                        { $push: { entries: { $each: [ entry ], $sort: { dateObject: 1 } } }}, { new: true } )
+                                        .populate( "entries" )
+                                        .then( ( updated ) => {  
+                                            console.log( 'WORKING UPDATED IS ', updated );
+                                            return updated;
+                                        } )
+                                }) 
                             } ) 
+                            .catch( err => err )
                     } )
                     .catch( err => err )
                 .catch( err => err )
             .catch( console.log( "Problems...probably not news" ) );
     })
+
+
+
+
 
     afterEach( () => {
         tearDownDb();
@@ -71,7 +100,31 @@ describe( "Tests the Timeline Router functionality", () => {
         closeServer( TEST_DATABASE_URL );
     } ); 
 
-    describe( "timeline creation", () => {
+    describe( " test for entry creation", () => {
+            
+        it( 'should create a new timeline for the user', () => {
+            const token = jwt.sign( { _id: accessibleUser._id, email: accessibleUser.email, userName: accessibleUser.name }, SECRET, { expiresIn: 10000 });
+            console.log( 'in first entry test.... ' );     
+        });
+    });
+
+
+
+
+
+
+
+
+
+})
+
+
+
+
+
+
+/*
+    describe( "entry creation", () => {
             
         it( 'should create a new timeline for the user', () => {
             const token = jwt.sign( { _id: accessibleUser._id, email: accessibleUser.email, userName: accessibleUser.name }, SECRET, { expiresIn: 10000 });
@@ -87,7 +140,7 @@ describe( "Tests the Timeline Router functionality", () => {
 
 
     
-    describe( "retrieve timeline", () => {
+    describe( "entry update", () => {
         it( 'should produce the required timeline by id', () => {
             const token = jwt.sign( { _id: accessibleUser._id, email: accessibleUser.email, userName: accessibleUser.name }, SECRET, { expiresIn: 10000 });
             return chai.request( app )
@@ -99,5 +152,22 @@ describe( "Tests the Timeline Router functionality", () => {
                 })
         })
     })
-})
+
+    describe( "entry deleted", () => {
+        it( 'should produce the required timeline by id', () => {
+            const token = jwt.sign( { _id: accessibleUser._id, email: accessibleUser.email, userName: accessibleUser.name }, SECRET, { expiresIn: 10000 });
+            return chai.request( app )
+                .post( '/api/timelines' )
+                .send( { timelineId: accessibleTimelineId } )
+                .set( 'Authorization', `Bearer ${ token }` )
+                .then( ( res ) => {
+                    expect( res ).to.be.json; 
+                })
+        })
+    })
+
+
+*/
+
+
 
